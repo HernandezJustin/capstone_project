12861adde5490ed5fb138cbcdbf15fde6046d6b7
Justin Hernandez <kaspersky102@aol.com>
2016-07-20 21:33:59 -0400

    added a form that interacts with the database

--- mini-capstone/app/controllers/products_controller.rb
+++ mini-capstone/app/controllers/products_controller.rb
@@ -1,5 +1,13 @@
 class ProductsController < ApplicationController
   def show
-    @products = Product.all
+    render 'show.html.erb'
+  end
+
+  def post
+    Product.create(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description])
+    render 'show.html.erb'
   end
 end
