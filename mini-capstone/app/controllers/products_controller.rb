c50b66ddbd87bbc4c4cedc1a02797894a6f41117
Justin Hernandez <kaspersky102@aol.com>
2016-07-27 18:41:33 -0400

    added model methods and changed database information via rails migrations

--- mini-capstone/app/controllers/products_controller.rb
+++ mini-capstone/app/controllers/products_controller.rb
@@ -1,13 +1,48 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render 'index.html.erb'
+  end
+
   def show
+    @product = Product.find_by(id: params[:id])
     render 'show.html.erb'
   end
 
-  def post
-    Product.create(
+  def new
+  end
+
+  def create
+    @product = Product.new(
       name: params[:name],
       price: params[:price],
+      image: params[:image],
       description: params[:description])
-    render 'show.html.erb'
+    @product.save
+    flash[:success] = "#{@product.name} has been created successfully"
+    redirect_to '/products'
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+    )
+    flash[:success] = "#{@product.name} has been updated successfully"
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    flash[:success] = "#{@product.name} has been deleted successfully"
+    redirect_to '/products'
   end
 end
--- mini-capstone/app/models/product.rb
+++ mini-capstone/app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_f < 2
+      'This item is discounted!'
+    else
+      'This item is not on sale!'
+    end
+  end
+
+  def tax
+    (price.to_f * 0.09).round(2)
+  end
+
+  def total
+    (price.to_f + tax).round(2)
+  end
 end
